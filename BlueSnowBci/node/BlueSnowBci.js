e04ff34355fe189dd3e9318865988ded8272cf76
tesla-cat <416640656@qq.com>
2019-12-17 11:43:06 +0800

    ..

--- BlueSnowBci/node/BlueSnowBci.js
+++ BlueSnowBci/node/BlueSnowBci.js
@@ -2,33 +2,48 @@
 class BlueSnowBci{
   constructor(){
     var self=this
-    self.esp32Ws='192.168.2.154:81'
+    self.settings={esp32Ws:'192.168.2.154:81'}
     const app=require('express')()
     const jsonParser=require('body-parser').json()
     app.get('/',(req,res)=>{res.sendFile(__dirname+'/client/index.html')})
     app.post('/',jsonParser,(req,res)=>{handleReq(req,res)})
     app.listen(3000,'localhost',()=>console.log("http://localhost:3000"))
+
     const WebSocketClient=require('websocket').client
     var esp32Client=new WebSocketClient()
-    esp32Client.on('connectFailed',(err)=>{
-      console.log(err); esp32Client.connect("ws://"+self.esp32Ws)})
+    esp32Client.on('connectFailed',(err)=>{console.log(err)
+      esp32Client.connect("ws://"+self.settings.esp32Ws)}
+    )
     esp32Client.on('connect',(connection)=>{
+      self.connection=connection
       connection.on('message',(message)=>{
-        self.registerPair("esp32",message.utf8Data)
+        self.record("esp32",message.utf8Data)
       })
-    }).connect("ws://"+self.esp32Ws)
+    }).connect("ws://"+self.settings.esp32Ws)
+
     self.ioHook=require('iohook').on("keypress",(e)=>{
-      self.registerPair("keypress",e.rawcode)
+      self.record("keypress",e.rawcode)
     }).start()
-    self.pair={esp32:null,keypress:null}
   }
 
   handleReq(req,res){
-
+    if(req.body.set){
+      self.connection.sendUTF(JSON.stringify(req.body.set.esp32))
+      self.settings=req.body.set.server
+    }
+    if(req.body.get){
+      res.send("fake data")
+    }
   }
-  registerPair(key,item){
+
+  record(key,item){
     var self=this
     // to do: create time stamp!
+  }
+
+  analyse(message){
+    var self=this
+    // to do: use python subprocess!
   }
 }
 
---
+++ BlueSnowBci/node/analyse.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(int argCnt,char** arg){
+  printf("argv[0] =  %s\n",arg[0]);
+  printf("argv[1] =  %s\n",arg[1]);
+  printf("argv[2] =  %s\n",arg[2]);
+  return 0;
+}
