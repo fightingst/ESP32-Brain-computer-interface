51280abb1bf031928f42bbd3cb89662bcef9d8cc
Ding Ruiqi <416640656@qq.com>
2019-12-30 00:14:52 +0800

    。。

--- BlueSnowBci/py/FFF.py
+++ BlueSnowBci/py/FFF.py
@@ -1,5 +1,24 @@
 import asyncio
 
+
+
+
+import websockets; import json
+import numpy as np
+from scipy import signal,fftpack
+import pyqtgraph as pg
+import time
+uri="ws://192.168.2.119:81"
+b,a=signal.iirnotch(50,30,1000)
+
+x = np.arange(1000)
+y = np.random.normal(size=(3, 1000))
+plotWidget = pg.plot(title="Three plot curves")
+for i in range(3):
+    plotWidget.plot(x, y[i], pen=(i,3))
+
+time.sleep(1000)
+
 ######################## keypress ########################
 from pynput import keyboard
 
@@ -9,15 +28,6 @@ def on_press(key):
 listener=keyboard.Listener(on_press=on_press).start()
 
 ######################## ws data ########################
-import websockets; import json
-import numpy as np
-from scipy import signal,fftpack
-import pyqtgraph as pg
-
-uri="ws://192.168.2.119:80"
-b,a=signal.iirnotch(50,30,1000)
-#plotWidget = pg.plot(title="filtered")
-x = np.arange(100)
 
 async def plot(y):
     plotWidget.plot(x, y[0], pen=(0,8),clear=True)
@@ -29,16 +39,17 @@ async def analyse(msg):
     filtered=signal.filtfilt(b,a,m["data"])
     ffted=np.abs(fftpack.fft(m["data"]))
     print("anal")
-    #await plot(filtered)
+    #await plot(z)
+    plotWidget.plot(x, filtered[0],clear=True)
 
 async def wsRoutine():
     async with websockets.connect(uri) as ws:
         while 1:
             msg = await ws.recv()
-            print(msg)
-            #await analyse(msg)
+            #print(msg)
+            await analyse(msg)
 
 ######################## main ########################
 async def main():
     await asyncio.gather(wsRoutine())
-asyncio.run(main())
+#asyncio.run(main())
--- BlueSnowBci/py/plt.py
+++ BlueSnowBci/py/plt.py
@@ -1,8 +1,55 @@
 import pyqtgraph as pg
 import numpy as np
-x = np.arange(1000)
-y = np.random.normal(size=(3, 1000))
+x = np.arange(100)
+y = np.random.normal(size=(3, 100))
+"""
 plotWidget = pg.plot(title="Three plot curves")
 for i in range(3):
-    plotWidget.plot(x, y[i], pen=(i,3),clear=True) ## setting pen=(i,3) automaticaly creates three different-colored pens
+    plotWidget.plot(x, y[i], pen=(i,3))
+"""
+import matplotlib.pyplot as plt
+import asyncio
+import websockets; import json
+from scipy import signal,fftpack
+import time
+
+uri="ws://192.168.2.119:81"
+b,a=signal.iirnotch(50,30,1000)
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+x = np.linspace(0, 6*np.pi, 100)
+y = 1e6*np.sin(x)
+line1, = ax.plot(x, y, 'r-')
+line2, = ax.plot(x, y, 'r-')
+
+"""
+async def plot(y):
+    plotWidget.plot(x, y[0], pen=(0,8),clear=True)
+    for i in range(7):
+        plotWidget.plot(x, y[i+1], pen=(i,8))
+"""
+async def analyse(msg):
+    m= json.loads(msg)
+    filtered=signal.filtfilt(b,a,m["data"])
+    ffted=np.abs(fftpack.fft(m["data"]))
+    print("anal")
+    line1.set_ydata(filtered[0])
+    line2.set_ydata(filtered[1])
+    fig.canvas.draw()
+    fig.canvas.flush_events()
     
+    #await plot(z)
+    #plotWidget.plot(x, filtered[0],clear=True)
+
+async def wsRoutine():
+    async with websockets.connect(uri) as ws:
+        while 1:
+            msg = await ws.recv()
+            #print(msg)
+            await analyse(msg)
+
+######################## main ########################
+async def main():
+    await asyncio.gather(wsRoutine())
+asyncio.run(main())
